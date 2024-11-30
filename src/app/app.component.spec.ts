9fbbcf1f1228e1ca5df8edaa4fc9b4255ac720e8
psmey <psmey-dev@protonmail.com>
2024-11-30 01:34:04 +0100

    fix linting

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -5,7 +5,7 @@ describe('AppComponent', () => {
   let spectator: Spectator<AppComponent>;
   const createComponent = createComponentFactory(AppComponent);
 
-  beforeEach(async () => spectator = createComponent());
+  beforeEach(async () => (spectator = createComponent()));
 
   it('should create the app', () => {
     expect(spectator.component).toBeTruthy();
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ScheduleComponent } from './components/schedule/schedule.component';
 
 @Component({
-    selector: 'app-root',
-    imports: [RouterOutlet],
-    templateUrl: './app.component.html',
-    styleUrl: './app.component.css',
+  selector: 'app-root',
+  imports: [RouterOutlet, ScheduleComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-    title = 'web_analytics_implementations';
+  title = 'web_analytics_implementations';
 }
