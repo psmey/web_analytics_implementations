ea7db021bd68d24a304185173084ef56ae43e7b0
psmey <psmey-dev@protonmail.com>
2024-12-02 12:42:37 +0100

    add toolbar to application

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,23 +1,13 @@
-import { AppComponent } from './app.component';
 import { Spectator, createComponentFactory } from '@ngneat/spectator/jest';
+import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   let spectator: Spectator<AppComponent>;
   const createComponent = createComponentFactory(AppComponent);
 
-  beforeEach(async () => (spectator = createComponent()));
+  beforeEach(() => (spectator = createComponent()));
 
   it('should create the app', () => {
     expect(spectator.component).toBeTruthy();
-  });
-
-  it(`should have the 'web_analytics_implementations' title`, () => {
-    expect(spectator.component.title).toEqual('web_analytics_implementations');
-  });
-
-  it('should render title', () => {
-    expect(spectator.query('h1')).toContainText(
-      'Hello, web_analytics_implementations'
-    );
   });
 });
