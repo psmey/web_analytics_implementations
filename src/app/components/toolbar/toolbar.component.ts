14b1017ca6bb0f4612e415d9605e28a46d7195e8
psmey <psmey-dev@protonmail.com>
2024-12-02 15:46:01 +0100

    implement nav list into sidenav

--- src/app/components/toolbar/toolbar.component.ts
+++ src/app/components/toolbar/toolbar.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
-import { MatListModule } from '@angular/material/list';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { RouterOutlet } from '@angular/router';
+import { NavigationListComponent } from '../navigation-list/navigation-list.component';
 
 @Component({
   selector: 'app-toolbar',
@@ -14,8 +14,8 @@ import { RouterOutlet } from '@angular/router';
     MatIconModule,
     MatSidenavModule,
     RouterOutlet,
-    MatListModule,
     MatIconModule,
+    NavigationListComponent,
   ],
   templateUrl: './toolbar.component.html',
   styleUrl: './toolbar.component.css',
