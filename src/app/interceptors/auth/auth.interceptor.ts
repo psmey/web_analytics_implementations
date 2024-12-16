05d7cf0797797b21ec4f7c90c92187f4ffebf666
psmey <psmey-dev@protonmail.com>
2024-12-16 17:49:35 +0100

    add authorization interceptor

---
+++ src/app/interceptors/auth/auth.interceptor.ts
@@ -0,0 +1,18 @@
+import { HttpEvent, HttpHandlerFn, HttpRequest } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+export function authInterceptor(
+  req: HttpRequest<unknown>,
+  next: HttpHandlerFn
+): Observable<HttpEvent<unknown>> {
+  const token = sessionStorage.getItem('token');
+
+  if (token) {
+    const authReq = req.clone({
+      setHeaders: { Authorization: `Bearer ${token}` },
+    });
+    return next(authReq);
+  }
+
+  return next(req);
+}
