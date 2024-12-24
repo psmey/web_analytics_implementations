b292c2050c9824535be29339ba91b4f6bf70d680
psmey <psmey-dev@protonmail.com>
2024-12-24 14:33:09 +0100

    generate add button

---
+++ src/app/add-button/add-button.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-button',
+  imports: [],
+  templateUrl: './add-button.component.html',
+  styleUrl: './add-button.component.css'
+})
+export class AddButtonComponent {
+
+}
