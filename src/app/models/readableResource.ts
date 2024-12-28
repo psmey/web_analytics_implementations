29af7fc04732eb9fe8e47c7ec04b264b766f94b1
psmey <psmey-dev@protonmail.com>
2024-12-28 22:52:39 +0100

    add logic for resource creation

---
+++ src/app/models/readableResource.ts
@@ -0,0 +1,7 @@
+export interface ReadableResource {
+  id: string;
+  street: string;
+  city: string;
+  country: string;
+  postalcode: string;
+}
--- src/app/pages/resources/resources.component.ts
+++ src/app/pages/resources/resources.component.ts
@@ -3,9 +3,17 @@ import { Component, inject, OnInit } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialog } from '@angular/material/dialog';
 import { MatIconModule } from '@angular/material/icon';
-import { IncomingResource, OutgoingResource } from '../../api/models';
+import { tap } from 'rxjs';
+import {
+  BidirectionalCoordinates,
+  IncomingResource,
+  OutgoingResource,
+} from '../../api/models';
 import { ResourceService } from '../../api/services';
 import { CreateResourceDialogComponent } from '../../components/create-resource-dialog/create-resource-dialog.component';
+import { Address } from '../../models/address';
+import { ReadableResource } from '../../models/readableResource';
+import { GeolocationService } from '../../services/geolocation/geolocation.service';
 
 @Component({
   selector: 'app-resources',
@@ -20,23 +28,35 @@ export class ResourcesComponent implements OnInit {
 
   private readonly matDialog: MatDialog = inject(MatDialog);
   private readonly resourceService: ResourceService = inject(ResourceService);
+  private readonly geolocationService: GeolocationService =
+    inject(GeolocationService);
 
   public ngOnInit(): void {
     this.loadResources();
   }
 
   public openDialog(): void {
-    const dialogRef = this.matDialog.open(CreateResourceDialogComponent);
-    dialogRef.afterClosed().subscribe((result: IncomingResource) => {
-      if (result) {
-        this.addResource(result);
-      }
-    });
+    const dialogRef = this.matDialog.open<
+      CreateResourceDialogComponent,
+      never,
+      ReadableResource
+    >(CreateResourceDialogComponent);
+
+    dialogRef
+      .afterClosed()
+      .subscribe(async (readableResource: ReadableResource | undefined) => {
+        if (readableResource) {
+          const resource = await this.parseResource(readableResource);
+          this.addResource(resource);
+        }
+      });
   }
 
   protected addResource(resource: IncomingResource): void {
-    this.resourceService.resourceCreateResource({ body: resource });
-    this.loadResources();
+    this.resourceService
+      .resourceCreateResource({ body: resource })
+      .pipe(tap(() => this.loadResources()))
+      .subscribe();
   }
 
   private loadResources(): void {
@@ -45,5 +65,24 @@ export class ResourcesComponent implements OnInit {
       .subscribe(
         (resources: OutgoingResource[]) => (this.resources = resources)
       );
+  }
+
+  private async parseResource(
+    resource: ReadableResource
+  ): Promise<IncomingResource> {
+    const address: Address = {
+      street: resource.street,
+      city: resource.city,
+      country: resource.country,
+      postalcode: parseInt(resource.postalcode),
+    };
+
+    const coordinates: BidirectionalCoordinates =
+      await this.geolocationService.toCoordinates(address);
+
+    return {
+      id: resource.id,
+      coordinates: coordinates,
+    };
   }
 }
