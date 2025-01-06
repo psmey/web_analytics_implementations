b48b5a08e70a1ca68fb3591831fe5f93f91847dc
psmey <psmey-dev@protonmail.com>
2025-01-06 15:45:12 +0100

    generate calls component

---
+++ src/app/pages/calls/calls.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-calls',
+  imports: [],
+  templateUrl: './calls.component.html',
+  styleUrl: './calls.component.css'
+})
+export class CallsComponent {
+
+}
