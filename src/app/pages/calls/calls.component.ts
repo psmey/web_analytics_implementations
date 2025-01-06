6ea9232d657e8bc72be8a567d5bbb052043ad33f
psmey <psmey-dev@protonmail.com>
2025-01-06 16:20:17 +0100

    implement loading calls on init

--- src/app/pages/calls/calls.component.ts
+++ src/app/pages/calls/calls.component.ts
@@ -1,11 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { OutgoingCall } from '../../api/models';
+import { CallService } from '../../api/services/call.service';
+import { TableComponent } from '../../components/table/table.component';
 
 @Component({
   selector: 'app-calls',
-  imports: [],
+  imports: [TableComponent],
   templateUrl: './calls.component.html',
-  styleUrl: './calls.component.css'
+  styleUrl: './calls.component.css',
 })
-export class CallsComponent {
+export class CallsComponent implements OnInit {
+  protected calls: OutgoingCall[] = [];
+  protected readonly columns: string[] = [];
 
+  private readonly callService: CallService = inject(CallService);
+
+  public ngOnInit() {
+    this.loadCalls();
+  }
+
+  private loadCalls() {
+    this.callService
+      .callGetCalls()
+      .subscribe((calls: OutgoingCall[]) => (this.calls = calls));
+  }
 }
