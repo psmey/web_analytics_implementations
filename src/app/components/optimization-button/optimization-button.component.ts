1828f1fb8b8fe239e512e2ee0f26546390ccfd6e
psmey <psmey-dev@protonmail.com>
2025-01-07 17:53:41 +0100

    generate optimization button component

---
+++ src/app/components/optimization-button/optimization-button.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-optimization-button',
+  imports: [],
+  templateUrl: './optimization-button.component.html',
+  styleUrl: './optimization-button.component.css',
+})
+export class OptimizationButtonComponent {}
